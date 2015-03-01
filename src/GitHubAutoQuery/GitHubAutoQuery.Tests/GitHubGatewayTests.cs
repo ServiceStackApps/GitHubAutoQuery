9bedc0615898923146ac08afaad3583eb8b74342
Demis Bellot <demis.bellot@gmail.com>
2015-03-01 17:19:35 -0500

    Add test for parsing Link HTTP Headers

---
+++ src/GitHubAutoQuery/GitHubAutoQuery.Tests/GitHubGatewayTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using GitHubAutoQuery.ServiceInterface;
+using NUnit.Framework;
+
+namespace GitHubAutoQuery.Tests
+{
+    [TestFixture]
+    public class GitHubGatewayTests
+    {
+        [Test]
+        public void Can_parse_link_urls()
+        {
+            var linkHeader = "<https://api.github.com/repositories/1339922/commits?page=101>; rel=\"next\", <https://api.github.com/repositories/1339922/commits?page=1>; rel=\"first\", <https://api.github.com/repositories/1339922/commits?page=99>; rel=\"prev\"";
+
+            var map = GithubGateway.ParseLinkUrls(linkHeader);
+
+            Assert.That(map, Is.EquivalentTo(new Dictionary<string,string>
+            {
+                {"next", "https://api.github.com/repositories/1339922/commits?page=101"},
+                {"first", "https://api.github.com/repositories/1339922/commits?page=1"},
+                {"prev", "https://api.github.com/repositories/1339922/commits?page=99"},
+            }));
+        } 
+    }
+}
