f0a10f33c918d74fa92a7baf973130d39c7be372
Demis Bellot <demis.bellot@gmail.com>
2015-03-02 15:38:33 -0500

    Remove 404 handler with more explicit FallbackToClientRoutes route

--- src/GitHubAutoQuery/GitHubAutoQuery.ServiceInterface/MyServices.cs
+++ src/GitHubAutoQuery/GitHubAutoQuery.ServiceInterface/MyServices.cs
@@ -1,17 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using ServiceStack;
-using GitHubAutoQuery.ServiceModel;
+﻿using ServiceStack;
 
 namespace GitHubAutoQuery.ServiceInterface
 {
+    [FallbackRoute("/{PathInfo*}")]
+    public class FallbackToClientRoutes
+    {
+        public string PathInfo { get; set; }
+    }
+
     public class MyServices : Service
     {
-        public object Any(Hello request)
+        public object Any(FallbackToClientRoutes request)
         {
-            return new HelloResponse { Result = "Hello, {0}!".Fmt(request.Name) };
+            return SinglePageApp();
+        }
+
+        public object SinglePageApp()
+        {
+            //Return default.cshtml for unmatched requests so routing can be handled on the client
+            return new HttpResult
+            {
+                View = "/default.cshtml"
+            };
         }
     }
 }
