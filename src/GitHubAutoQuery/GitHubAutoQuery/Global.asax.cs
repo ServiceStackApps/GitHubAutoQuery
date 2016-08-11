1c25711bbbf62c1805860d31a78f5556ec416eeb
Demis Bellot <demis.bellot@gmail.com>
2016-08-11 16:16:25 -0400

    Upgrade to v4.0.63 inc. new saved queries feature

--- src/GitHubAutoQuery/GitHubAutoQuery/Global.asax.cs
+++ src/GitHubAutoQuery/GitHubAutoQuery/Global.asax.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.SessionState;
-using ServiceStack;
 
 namespace GitHubAutoQuery
 {
@@ -12,7 +6,6 @@ namespace GitHubAutoQuery
     {
         protected void Application_Start(object sender, EventArgs e)
         {
-            Licensing.RegisterLicenseFromFileIfExists("~/appsettings.license.txt".MapHostAbsolutePath());
             new AppHost().Init();
         }
     }
